25199491f32569e1f70ec45a05bbf016a158b7e7
Youssef Mokhtar <99183756+mikha03@users.noreply.github.com>
2022-11-03 21:51:40 +0200

    Delete DataAcquisition/text directory

--- DataAcquisition/text/dio.h
+++
@@ -1,4 +0,0 @@
-void Dio_init();
-void Dio_SetPinDirection(unsigned char Port, unsigned char Pin, unsigned char Direction);
-void Dio_SetPinState(unsigned char Port,unsigned char Pin, unsigned char State);
-unsigned char Dio_GetPinState(unsigned char Port,unsigned char Pin);
